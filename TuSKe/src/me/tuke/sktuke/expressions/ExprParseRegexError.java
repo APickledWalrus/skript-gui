660d03630eb5d38547071c888945f32ccabfb74e
Tuke_Nuke <leandro.p.alencar@gmail.com>
2016-12-06 13:20:59 -0200

    new expression to return the last regex parse error

---
+++ TuSKe/src/me/tuke/sktuke/expressions/ExprParseRegexError.java
@@ -0,0 +1,40 @@
+package me.tuke.sktuke.expressions;
+
+import org.bukkit.event.Event;
+import javax.annotation.Nullable;
+
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+
+public class ExprParseRegexError extends SimpleExpression<String>{
+
+	public static String parserError = null;
+	@Override
+	public Class<? extends String> getReturnType() {
+		return String.class;
+	}
+
+	@Override
+	public boolean isSingle() {
+		return true;
+	}
+
+	@Override
+	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event arg0, boolean arg1) {
+		return "last regex parser error";
+	}
+
+	@Override
+	@Nullable
+	protected String[] get(Event arg0) {
+		return new String[]{parserError};
+	}
+
+}
