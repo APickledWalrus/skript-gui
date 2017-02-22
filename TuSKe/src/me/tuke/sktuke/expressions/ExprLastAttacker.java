9ae63e433f246d4de80d238111f08cb8c3b39e7f
Tuke-Nuke <leandro.p.alencar@gmail.com>
2017-02-22 13:53:39 -0300

    Return the shooter of projectile and the last attacker of a vehicle

--- TuSKe/src/me/tuke/sktuke/expressions/ExprLastAttacker.java
+++ TuSKe/src/me/tuke/sktuke/expressions/ExprLastAttacker.java
@@ -2,10 +2,14 @@ package me.tuke.sktuke.expressions;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.vehicle.VehicleDamageEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
 
+import ch.njol.skript.bukkitutil.ProjectileUtils;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
@@ -42,8 +46,24 @@ public class ExprLastAttacker extends SimpleExpression<Object> {
 		if (entity != null && entity.getLastDamageCause() != null){
 			if (entity.getLastDamageCause() instanceof EntityDamageByBlockEvent)
 				return new Block[]{((EntityDamageByBlockEvent)entity.getLastDamageCause()).getDamager()};
-			if (entity.getLastDamageCause() instanceof EntityDamageByEntityEvent)
-				return new Entity[]{((EntityDamageByEntityEvent)entity.getLastDamageCause()).getDamager()};
+			return new Entity[]{getAttacker(entity.getLastDamageCause())};
+		}
+		return null;
+	}
+
+	public static Entity getAttacker(final Event e) {
+		if (e instanceof EntityDamageByEntityEvent) {
+			if (((EntityDamageByEntityEvent) e).getDamager() instanceof Projectile) {
+				final Object o = ProjectileUtils.getShooter((Projectile) ((EntityDamageByEntityEvent) e).getDamager());
+				if (o instanceof Entity)
+					return (Entity) o;
+				return null;
+			}
+			return ((EntityDamageByEntityEvent) e).getDamager();
+		} else if (e instanceof VehicleDamageEvent) {
+			return ((VehicleDamageEvent) e).getAttacker();
+		} else if (e instanceof VehicleDestroyEvent) {
+			return ((VehicleDestroyEvent) e).getAttacker();
 		}
 		return null;
 	}
