7b35cb9842fad25ec3a7b84a6a6957f84dad3ec6
Fusezion <fusezionstream@gmail.com>
2022-08-22 00:59:50 -0400

    Duplication Fix

--- src/main/java/io/github/apickledwalrus/skriptgui/gui/events/GUIEvents.java
+++ src/main/java/io/github/apickledwalrus/skriptgui/gui/events/GUIEvents.java
@@ -1,6 +1,8 @@
 package io.github.apickledwalrus.skriptgui.gui.events;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.SkriptEventHandler;
+import com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent;
 import io.github.apickledwalrus.skriptgui.SkriptGUI;
 import io.github.apickledwalrus.skriptgui.gui.GUI;
 import org.bukkit.GameMode;
@@ -20,6 +22,10 @@ public class GUIEvents implements Listener {
 		SkriptEventHandler.listenCancelled.add(InventoryDragEvent.class);
 		SkriptEventHandler.listenCancelled.add(InventoryOpenEvent.class);
 		SkriptEventHandler.listenCancelled.add(InventoryCloseEvent.class);
+		if (Skript.classExists("com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent")) {
+			// We need to track this event (see https://github.com/APickledWalrus/skript-gui/issues/33)
+			SkriptEventHandler.listenCancelled.add(PlayerRecipeBookClickEvent.class);
+		}
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
@@ -153,6 +159,14 @@ public class GUIEvents implements Listener {
 		GUI gui = SkriptGUI.getGUIManager().getGUI(e.getInventory());
 		if (gui != null) {
 			gui.getEventHandler().onClose(e);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onRecipeBookClick(PlayerRecipeBookClickEvent e) {
+		GUI gui = SkriptGUI.getGUIManager().getGUI(e.getPlayer());
+		if (gui != null) {
+			e.setCancelled(true);
 		}
 	}
 
