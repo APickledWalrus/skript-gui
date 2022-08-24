028732cc1b58e7f1518ba836668d7e9c64238e77
Fusezion <fusezionstream@gmail.com>
2022-08-23 20:24:33 -0400

    Added @EventHandler

--- src/main/java/io/github/apickledwalrus/skriptgui/gui/events/RecipeEvent.java
+++ src/main/java/io/github/apickledwalrus/skriptgui/gui/events/RecipeEvent.java
@@ -4,6 +4,8 @@ import ch.njol.skript.SkriptEventHandler;
 import com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent;
 import io.github.apickledwalrus.skriptgui.SkriptGUI;
 import io.github.apickledwalrus.skriptgui.gui.GUI;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class RecipeEvent implements Listener {
@@ -13,6 +15,7 @@ public class RecipeEvent implements Listener {
 		SkriptEventHandler.listenCancelled.add(PlayerRecipeBookClickEvent.class);
 	}
 
+	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onRecipeBookClick(PlayerRecipeBookClickEvent event) {
 		GUI gui = SkriptGUI.getGUIManager().getGUI(event.getPlayer().getOpenInventory().getTopInventory());
 		if (gui != null) {
