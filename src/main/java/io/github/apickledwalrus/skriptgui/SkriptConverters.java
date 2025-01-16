e5d86675fe9a80d074c829f4dff17531ddcc9682
APickledWalrus <apickledwalrus@gmail.com>
2025-01-16 14:15:53 -0500

    Begin updating for Skript 2.10

--- src/main/java/io/github/apickledwalrus/skriptgui/SkriptConverters.java
+++ src/main/java/io/github/apickledwalrus/skriptgui/SkriptConverters.java
@@ -1,8 +1,8 @@
 package io.github.apickledwalrus.skriptgui;
 
-import ch.njol.skript.registrations.Converters;
 import io.github.apickledwalrus.skriptgui.gui.GUI;
 import org.bukkit.inventory.Inventory;
+import org.skriptlang.skript.lang.converter.Converters;
 
 public class SkriptConverters {
 
